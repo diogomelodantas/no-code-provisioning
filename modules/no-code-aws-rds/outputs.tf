d037d1fbc58c320dc98230935666dba35cef7edd
Diogo Dantas <diogomelo@gmail.com>
2026-05-08 15:24:22 -0300

    feat: Update RDS hostname output to include '-test' suffix for clarity

--- modules/no-code-aws-rds/outputs.tf
+++ modules/no-code-aws-rds/outputs.tf
@@ -8,7 +8,7 @@ output "region" {
 
 output "rds_hostname" {
   description = "RDS instance hostname."
-  value       = aws_db_instance.education.address
+  value       = "${aws_db_instance.education.address}-test"
 }
 
 output "rds_port" {
